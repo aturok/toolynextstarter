33c2422bf41f9e2bd826716fbabb819a78cea3f9
Alex Turok <chaot8@gmail.com>
2020-09-21 19:46:43 +0300

    add og attributes

--- components/Common/Head.tsx
+++ components/Common/Head.tsx
@@ -1,10 +1,17 @@
 import styled from 'styled-components';
 import NextHead from 'next/head'
+import * as constants from '../../constants/constants';
 
 export default () => {
     return (
         <NextHead>
-            <title>Your best website for your unique idea!</title>
+            <title>{constants.siteTitle}</title>
+            <meta property="og:title" content={constants.siteTitle} />
+            <meta property="og:site_name" content={constants.siteTitle} />
+            <meta property="og:description" content={constants.siteDescription} />
+            <meta property="og:type" content={constants.ogType} />
+            <meta property="og:url" content={constants.ogUrl} />
+            <meta property="og:image" content={constants.ogImage} />
             
             <link rel="icon" href="/favicon.ico" />
             {/* use favicon generator to create a favicon from an image */}
--- constants/constants.ts
+++ constants/constants.ts
@@ -1,2 +1,7 @@
 export const emailaddress = 'test@toolympus.com';
 export const facebooklink = 'https://www.facebook.com/skllledapp';
+export const siteTitle = "Your best website for your unique idea!";
+export const siteDescription = "A beautiful (not really) template for a simple website in NextJS";
+export const ogImage = "https://yourcoolwebsite.com/sharing/site/img";
+export const ogType = "website";
+export const ogUrl = "https://yourcoolwebsite.com";
